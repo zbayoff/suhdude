ad0d141a729f32e76c10680beddaf81f118c820c
zbayoff <zrbayoff@gmail.com>
2020-03-21 11:28:21 -0400

    update

--- app.js
+++ app.js
@@ -50,7 +50,10 @@ app.use(
 	})
 );
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+// app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 app.use('/groupmeApi', groupmeRoutes);
 app.use('/api', suhdudeRoutes);
