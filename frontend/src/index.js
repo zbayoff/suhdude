fe1a76d9b45d66242739504ddf2da73346e96aec
zbayoff <zrbayoff@gmail.com>
2020-03-21 12:08:57 -0400

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'typeface-roboto';
 
 const app = (
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>
+	</HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
