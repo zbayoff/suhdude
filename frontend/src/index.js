0005c98cea4774d24ee970751d454f7790539264
zbayoff <zrbayoff@gmail.com>
2021-01-27 10:32:52 -0500

    add RUM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,16 @@ import { HashRouter } from 'react-router-dom';
 import 'typeface-roboto';
 
 import { datadogLogs } from '@datadog/browser-logs'
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '8431fa34-0dc3-4da0-bc0e-8e32c50a3418',
+    clientToken: 'pub5d2ec4cb918f9309a6a4153f7e943fb6',
+    site: 'datadoghq.com',
+    service: 'suhdude',
+    sampleRate: 100,
+    trackInteractions: true
+});
 
 datadogLogs.init({
 	clientToken: 'pub04fa4edf13403c58b97b2d4403403911',
