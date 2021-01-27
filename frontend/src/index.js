3215c829cacc53d8ad5a2f58e67f34d4338442ed
zbayoff <zrbayoff@gmail.com>
2021-01-27 10:23:21 -0500

    add browser logs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import 'typeface-roboto';
 
+import { datadogLogs } from '@datadog/browser-logs'
+
+datadogLogs.init({
+	clientToken: 'pub04fa4edf13403c58b97b2d4403403911',
+	forwardErrorsToLogs: true,
+	sampleRate: 100,
+  })
+
 const app = (
 	<HashRouter>
 		<App />
